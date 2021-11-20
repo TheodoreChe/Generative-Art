bca4d0faf7e0c03cafa35d3abe550048f29be5f2
theodoreche <2226390@gmail.com>
2021-11-20 18:53:23 +0100

    🐛 fix build config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { merge } = require('webpack-merge')
 const baseConfig = require('./webpack.common.js')
 const paths = require('./paths')
@@ -12,6 +13,33 @@ module.exports = merge(baseConfig, {
     publicPath: '/',
     filename: 'js/[name].[contenthash].bundle.js',
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: false,
+              modules: false,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // Extracts CSS into separate files
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 
   optimization: {
     minimize: true,
